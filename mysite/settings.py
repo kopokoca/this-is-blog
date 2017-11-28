2d0308372a3e6ee52ee538c1823de3a599b5f594
kopokoca <kopokoca1@yandex.ru>
2017-11-28 05:52:52 +0300

    edited settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2r7zciru#i7x89=9902g7d4x1^35^0=@^avi1%%wkf5rx4cabd'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<user>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'kopokoca.pythonanywhere.com']
 
 
 # Application definition
